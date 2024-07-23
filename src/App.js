55757c119b4afc036f9a7a6d6ef05016242651c3
ehabdalank <ehabdalank018@gmail.com>
2024-07-23 12:08:39 +0300

    Mangoapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+import Axios from "axios";
 
 function App() {
+  const [ListOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/creatUser").then((response) => {
+      alert("created");
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="userdisplay">
+        {ListOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>age: {user.age}</h1>
+              <h1>username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <input type="text " placeholder="name"></input>
+        <input type="text " placeholder="age"></input>
+        <input type="text " placeholder="username"></input>
+        <button onClick={createUser}>CreateUser</button>
+      </div>
     </div>
   );
 }
